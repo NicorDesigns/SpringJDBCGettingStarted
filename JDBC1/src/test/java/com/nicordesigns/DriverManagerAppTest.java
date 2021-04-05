2cfe717600f2c4f2083f971a9865d9d636d3f442
NicorDesigns <nicocpk@gmail.com>
2021-04-05 16:03:01 -0400

    DriverManagerApp Rename

---
+++ JDBC1/src/test/java/com/nicordesigns/DriverManagerAppTest.java
@@ -0,0 +1,18 @@
+package com.nicordesigns;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertTrue;
+
+/**
+ * Unit test for simple DriverManagerApp.
+ */
+public class DriverManagerAppTest {
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue() {
+        assertTrue(true);
+    }
+}
