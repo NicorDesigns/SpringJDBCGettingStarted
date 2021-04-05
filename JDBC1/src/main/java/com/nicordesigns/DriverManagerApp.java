1cb14478a98f71c207d44b8e4221104a00ddeb81
NicorDesigns <nicocpk@gmail.com>
2021-04-05 16:20:51 -0400

    DriverManagerApp Build Unit Test and Update POMs

--- JDBC1/src/main/java/com/nicordesigns/DriverManagerApp.java
+++ JDBC1/src/main/java/com/nicordesigns/DriverManagerApp.java
@@ -1,10 +1,98 @@
 package com.nicordesigns;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.Properties;
+
 /**
  * Hello world!
  */
 public class DriverManagerApp {
-    public static void main(String[] args) {
-        System.out.println("Hello World!");
+
+  private String userName;
+  private String password;
+  private String dbms;
+  private String serverName;
+  private String portNumber;
+  private String dbName;
+
+  public static void main(String[] args) {
+    System.out.println("Hello World!");
+  }
+
+  public String getUserName() {
+    return userName;
+  }
+
+  public void setUserName(String userName) {
+    this.userName = userName;
+  }
+
+  public String getPassword() {
+    return password;
+  }
+
+  public void setPassword(String password) {
+    this.password = password;
+  }
+
+  public String getDbms() {
+    return dbms;
+  }
+
+  public void setDbms(String dbms) {
+    this.dbms = dbms;
+  }
+
+  public String getServerName() {
+    return serverName;
+  }
+
+  public void setServerName(String serverName) {
+    this.serverName = serverName;
+  }
+
+  public String getPortNumber() {
+    return portNumber;
+  }
+
+  public void setPortNumber(String portNumber) {
+    this.portNumber = portNumber;
+  }
+
+  public String getDbName() {
+    return dbName;
+  }
+
+  public void setDbName(String dbName) {
+    this.dbName = dbName;
+  }
+
+  public Connection getConnection() {
+    Connection conn = null;
+    Properties connectionProps = new Properties();
+    connectionProps.put("user", this.userName);
+    connectionProps.put("password", this.password);
+
+    if (this.dbms.equals("mariadb")) {
+      try {
+        conn =
+                DriverManager.getConnection(
+                        "jdbc:"
+                                + this.dbms
+                                + "://"
+                                + this.serverName
+                                + ":"
+                                + this.portNumber
+                                + "/"
+                                + this.dbName,
+                        connectionProps);
+      } catch (SQLException throwables) {
+        throwables.printStackTrace();
+      }
     }
+    System.out.println("Connected to database");
+    return conn;
+  }
 }
--- JDBC1/src/test/java/com/nicordesigns/DriverManagerAppTest.java
+++ JDBC1/src/test/java/com/nicordesigns/DriverManagerAppTest.java
@@ -1,18 +1,40 @@
 package com.nicordesigns;
 
-import org.junit.Test;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 
-import static org.junit.Assert.assertTrue;
+import java.sql.Connection;
+import java.sql.SQLException;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 /**
  * Unit test for simple DriverManagerApp.
  */
 public class DriverManagerAppTest {
-    /**
-     * Rigorous Test :-)
-     */
-    @Test
-    public void shouldAnswerWithTrue() {
-        assertTrue(true);
+
+  private final DriverManagerApp driverManagerApp = new DriverManagerApp();
+
+  @BeforeEach
+  void setUpDBConnection() {
+    driverManagerApp.setDbms("mariadb");
+    driverManagerApp.setUserName("root");
+    driverManagerApp.setPassword("secret");
+    driverManagerApp.setServerName("localhost");
+    driverManagerApp.setPortNumber("3306");
+    driverManagerApp.setDbName("charityDB");
+  }
+
+  @Test
+  void getConnection() {
+
+    try (Connection sqlConnection = driverManagerApp.getConnection()) {
+      System.out.println("SQL Connection Catalog: " + sqlConnection.getCatalog());
+      assertEquals("charityDB", sqlConnection.getCatalog());
+    } catch (SQLException throwable) {
+      throwable.printStackTrace();
     }
+
+
+  }
 }
