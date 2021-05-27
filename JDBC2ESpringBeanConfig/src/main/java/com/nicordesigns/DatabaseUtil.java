4d18fac4f33a7878f92c0ec4d8f3dacc0a3e8965
NicorDesigns <nicocpk@gmail.com>
2021-05-27 17:37:58 -0400

    Getting the data source from the Spring XML Context

--- JDBC2ESpringBeanConfig/src/main/java/com/nicordesigns/DatabaseUtil.java
+++ JDBC2ESpringBeanConfig/src/main/java/com/nicordesigns/DatabaseUtil.java
@@ -1,49 +1,20 @@
 package com.nicordesigns;
 
-import org.mariadb.jdbc.MariaDbPoolDataSource;
-
+import javax.sql.DataSource;
 import java.sql.Connection;
 import java.sql.SQLException;
 import java.util.Objects;
 
 public class DatabaseUtil {
 
-  private final String userName;
-  private final String password;
-  private final String dbms;
-  private final String serverName;
-  private final String portNumber;
-  private final String dbName;
+  private final DataSource dataSource;
 
-  public DatabaseUtil(
-      String userName,
-      String password,
-      String dbms,
-      String serverName,
-      String portNumber,
-      String dbName) {
-    this.userName = userName;
-    this.password = password;
-    this.dbms = dbms;
-    this.serverName = serverName;
-    this.portNumber = portNumber;
-    this.dbName = dbName;
+  public DatabaseUtil(DataSource dataSource) {
+    this.dataSource = dataSource;
   }
 
   private Connection createConnection() throws SQLException {
-    MariaDbPoolDataSource mariaDbPoolDataSource = new MariaDbPoolDataSource();
-    Connection conn;
-    mariaDbPoolDataSource.setServerName(this.serverName);
-    mariaDbPoolDataSource.setPortNumber(Integer.parseInt("3306"));
-    mariaDbPoolDataSource.setUser(this.userName);
-    mariaDbPoolDataSource.setPassword(this.password);
-    mariaDbPoolDataSource.setDatabaseName(this.dbms);
-    mariaDbPoolDataSource.setMaxPoolSize(10);
-    mariaDbPoolDataSource.setPoolName("JDBC3");
-    mariaDbPoolDataSource.setUrl("jdbc:mariadb://localhost:3306/charityDB");
-    conn = mariaDbPoolDataSource.getConnection();
-
-    return conn;
+    return dataSource.getConnection();
   }
 
   public String getCatalogName() {
--- JDBC2ESpringBeanConfig/src/main/java/com/nicordesigns/HelloServlet.java
+++ JDBC2ESpringBeanConfig/src/main/java/com/nicordesigns/HelloServlet.java
@@ -1,17 +1,28 @@
 package com.nicordesigns;
 
+import org.springframework.web.context.support.XmlWebApplicationContext;
+
 import javax.servlet.ServletOutputStream;
+import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.sql.DataSource;
 import java.io.IOException;
 
+@WebServlet("/HelloServlet")
 public class HelloServlet extends HttpServlet {
 
   @Override
   protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {
-    DatabaseUtil databaseUtil =
-        new DatabaseUtil("root", "secret", "mariadb", "localhost", "3306", "charityDB");
+    XmlWebApplicationContext context = new XmlWebApplicationContext();
+    context.setConfigLocation("/WEB-INF/applicationContext.xml");
+    context.setServletContext(req.getServletContext());
+    context.refresh();
+
+    DataSource ds = (DataSource) context.getBean("dataSource");
+
+    DatabaseUtil databaseUtil = new DatabaseUtil(ds);
     String catalogName;
     catalogName = databaseUtil.getCatalogName();
     ServletOutputStream out = resp.getOutputStream();
