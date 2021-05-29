6af9386582359b5c07c7c0b40971389ce853a39f
NicorDesigns <nicocpk@gmail.com>
2021-05-29 16:22:46 -0400

    JDBC1ModuleFinish commit

--- JDBC1/src/main/java/com/nicordesigns/DriverManagerApp.java
+++ JDBC1/src/main/java/com/nicordesigns/DriverManagerApp.java
@@ -5,7 +5,9 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 import java.util.Properties;
 
-/** Hello world! */
+/***
+ * DriverManagerApp
+ */
 public class DriverManagerApp {
 
   private String userName;
@@ -19,48 +21,24 @@ public class DriverManagerApp {
     System.out.println("Hello World!");
   }
 
-  public String getUserName() {
-    return userName;
-  }
-
   public void setUserName(String userName) {
     this.userName = userName;
-  }
-
-  public String getPassword() {
-    return password;
   }
 
   public void setPassword(String password) {
     this.password = password;
   }
 
-  public String getDbms() {
-    return dbms;
-  }
-
   public void setDbms(String dbms) {
     this.dbms = dbms;
-  }
-
-  public String getServerName() {
-    return serverName;
   }
 
   public void setServerName(String serverName) {
     this.serverName = serverName;
   }
 
-  public String getPortNumber() {
-    return portNumber;
-  }
-
   public void setPortNumber(String portNumber) {
     this.portNumber = portNumber;
-  }
-
-  public String getDbName() {
-    return dbName;
   }
 
   public void setDbName(String dbName) {
@@ -86,8 +64,8 @@ public class DriverManagerApp {
                     + "/"
                     + this.dbName,
                 connectionProps);
-      } catch (SQLException throwables) {
-        throwables.printStackTrace();
+      } catch (SQLException throwable) {
+        throwable.printStackTrace();
       }
     }
     System.out.println("Connected to database");
